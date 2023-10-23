2ac3704724be050b28c97a40320b0e35d91077c2
Thomas Pritchard <tp4618@ic.ac.uk>
2023-10-23 15:36:11 +0100

    Updated app.py style

--- api/app.py
+++ api/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
@@ -12,4 +16,9 @@ def submit():
     input_sort = request.form.get("sortcode")
     input_account = request.form.get("accountnumber")
     input_pg = request.form.get("pgnum")
-    return render_template("hello.html", name=input_name, age=input_age, sort=input_sort, acc=input_account, pg=input_pg)
+    return render_template("hello.html",
+                           name=input_name,
+                           age=input_age,
+                           sort=input_sort,
+                           acc=input_account,
+                           pg=input_pg)
