ecae47535e9743c119ac61a6029d1f7fd08bc3c4
Varun Khandekar <varun.khandekar23@imperial.ac.uk>
2023-11-06 15:30:56 +0000

    restoring old version of hello_world

--- api/app.py
+++ api/app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    q = request.args.get("q")
-    return process_query(q)
+    #q = request.args.get("q")
+    #return process_query(q)
+    return render_template("index.html")
 
 
 # @app.route("/query?q=dinosaurs", methods=["GET"])
