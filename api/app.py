1cca1346a7e8e47b6cc14e45c2a293708a7dc304
Thomas Pritchard <tp4618@ic.ac.uk>
2023-11-06 17:35:07 +0000

    full table 1

--- api/app.py
+++ api/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from datetime import datetime
+import pytz
 
 import requests
 
@@ -42,43 +44,49 @@ def submit():
 @app.route("/github_form/submit", methods=["POST"])
 def submit_github():
     input_username = request.form.get("github_username")
-    response = \
-        (requests.get(f"https://api.github.com/users/{input_username}/repos"))
+    response = requests.get(f'https://api.github.com/users/{input_username}/repos')
 
     if response.status_code == 200:
         repos = response.json()
         for repo in repos:
-            repo_commit_details = requests.get(
-                f'https://api.github.com/repos/{repo["full_name"]}/commits'
-            )
+            repo_commit_details = requests.get(f'https://api.github.com/repos/{repo["full_name"]}/commits')
 
             if repo_commit_details.status_code == 200:
                 commits = repo_commit_details.json()
-                # Safety check if the commits list is empty
                 if commits:
-                    # Assign the SHA to a new key in the repo dictionary.
+                    # Get the first commit's data
+                    commit_data = commits[0]["commit"]
+                    author_data = commit_data["author"]
+
+                    # Format the date string to a more readable format
+                    date_str = author_data["date"]
+                    date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
+                    date_obj = date_obj.replace(tzinfo=pytz.utc)  # Attach UTC timezone information
+
+                    # Here you can adjust the date format as per your requirement
+                    formatted_date = date_obj.strftime("%B %d, %Y %H:%M:%S UTC")
+
+                    # Add the cleaned up data to the repo dictionary
                     repo["special_sha"] = commits[0]["sha"]
+                    repo["author_name"] = author_data["name"]
+                    repo["author_email"] = author_data["email"]
+                    repo["commit_date"] = formatted_date
                 else:
                     repo["special_sha"] = "No commits available"
+                    repo["author_name"] = ""
+                    repo["author_email"] = ""
+                    repo["commit_date"] = ""
             else:
                 repo["special_sha"] = "Error fetching commits"
-
-        return render_template(
-            "github_form_post.html",
-            github_username=input_username,
-            github_repos=repos
-        )
+                repo["author_name"] = ""
+                repo["author_email"] = ""
+                repo["commit_date"] = ""
+        return render_template("github_form_post.html", github_username=input_username, github_repos=repos)
     else:
         repos = None
-        error_message =\
-            ("Failed to fetch repositories. "
-             "Please check the GitHub username and try again.")
-        return render_template(
-            "github_form_post.html",
-            github_username=input_username,
-            github_repos=repos,
-            error_message=error_message,
-        )
+        error_message = "Failed to fetch repositories. Please check the GitHub username and try again."
+        return render_template("github_form_post.html", github_username=input_username, github_repos=repos,
+                               error_message=error_message)
 
 
 def process_query(word):
